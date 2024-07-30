731760d7f8f21723726d79e9426008bb943a3606
Rahul Kumar <rahul@gmail.com>
2024-07-30 08:01:42 +0545

    final commit

--- app/src/main/java/com/example/ismt_database/MainActivity.java
+++ app/src/main/java/com/example/ismt_database/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ismt_database;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,6 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -51,5 +53,57 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        btn_viewData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor=myDB.getAllData();
+                if (cursor.getCount()==0){
+                    Toast.makeText(MainActivity.this, "Empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                StringBuffer buffer=new StringBuffer();
+                while (cursor.moveToNext()){
+                    buffer.append("Id"+cursor.getString(0)+"\n");
+                    buffer.append("name"+cursor.getString(1)+"\n");
+                    buffer.append("marks"+cursor.getString(2)+"\n");
+                }
+                show("Data",buffer.toString());
+            }
+        });
+
+        btn_editData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isUpdate=myDB.updateData(
+                        txt_id.getText().toString().trim(),
+                        txt_name.getText().toString().trim(),
+                        txt_marks.getText().toString().trim());
+                if (isUpdate==true){
+                    Toast.makeText(MainActivity.this, "Data Updated", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "Data Not Update", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+      btn_deleteData.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Integer deleteData=myDB.deleteData(txt_id.getText().toString().trim());
+              if (deleteData>0){
+                  Toast.makeText(MainActivity.this, "Data Deleted ", Toast.LENGTH_SHORT).show();
+              }else {
+                  Toast.makeText(MainActivity.this, "Deta has No Deleted", Toast.LENGTH_SHORT).show();
+              }
+          }
+      });
+    }
+    public void show(String title,String Message){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
     }
 }
